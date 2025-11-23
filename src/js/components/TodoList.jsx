6a6fdd4e607785ef75c5cd75f6647bf0f3c0c781
cinjude <110071132+cinjude@users.noreply.github.com>
2025-11-23 04:47:03 +0000

    [todo]

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -4,124 +4,115 @@ import Home from "./Home";
 
 const ToddoList = () => {
 
-
     const [imputField, setImputfield] = useState("")
     const [tasks, setNewTask] = useState([])
+    const API_URL = 'https://playground.4geeks.com/todo'
 
-    const showlist = () => {
-
-        fetch('https://playground.4geeks.com/todo/users/judelin', {
-
+    const createUsers = () => {
+        fetch(API_URL + "/users/cintilmon", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }
         })
-            .then(resp => {
-                console.log(resp.ok); // Será true si la respuesta es exitosa
-                console.log(resp.status); // El código de estado 201, 300, 400, etc.
-                return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-            })
-            .then(data => {
-                setNewTask(data.todos)
-                console.log(tasks)
-                // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-                console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-            })
-            .catch(error => {
-                // Manejo de errores
-                console.log(error);
-            });
-
+            .then(resp => console.log(resp))
+            .then(data => console.log(data))
+            .then(error => console.log(error))
     }
+
+    const bringList = () => {
+        fetch(API_URL + "/users/cintilmon")
+            .then((resp) => {
+                if (resp.status === 404) {
+                    createUsers()
+                }
+                return resp.json()
+            })
+            .then((data) => {
+                console.log(data.todos)
+                setNewTask(data.todos)
+            })
+            .then(error => console.log(error))
+    };
 
     useEffect(() => {
-        showlist()
+        bringList()
     }, [])
-
-    const imputChange = (event) => {
-        setImputfield(event.target.value)
-    }
 
     const handleTouch = (e) => {
         if (e.key === "Enter" && imputField.trim() !== "") {
-            // const newtask = { label: 'hola', is_done: false }
-
-            fetch('https://playground.4geeks.com/todo/todos/judelin', {
+            fetch(API_URL + "/todos/cintilmon", {
                 method: "POST",
                 body: JSON.stringify({
-                    label: imputField,
-                    is_done: false
+                    "label": imputField,
+                    "is_done": false,
                 }),
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-type": "application/json"
                 }
             })
-                .then(resp => {
-                    console.log(resp.ok);
+                .then((resp) => {
+                    console.log(resp)
                     if (resp.ok) {
-                        // onLoad()
                         setImputfield("")
                     }
-                    console.log(resp.status);
-                    return resp.json();
+                    console.log(resp.status)
+                    return resp.json()
                 })
-                .then(data => {
-                    console.log(data);
+                .then((data) => {
+                    console.log(data)
                     setNewTask([...tasks, data])
                 })
-                .catch(error => {
-                    console.log(error);
-                });
+                .catch(error => console.log(error))
         }
     }
 
-    function addTask() {
-        if (imputField.trim() === "") {
-            return alert("Write before you add it")
-        }
-        setNewTask(texte => [...texte, imputField])
-        setImputfield("")
-    }
-
-
-    const deletetask = (id) => {
-        const upDateTask = tasks.filter((task) => task.id !== id);
-        setNewTask(upDateTask)
-    }
-
-    const deleteTask = async (id) => {
-        try {
-            const res = await fetch( `https://playground.4geeks.com/todo/todos/judelin ${id}`, {
-                method: "DELETE",
-            });
-
-            if (!res.ok) {
-                throw new Error(`Failed to delete task with id ${id}`);
+    const deleteTask = (id) => {
+        fetch(API_URL + "/todos/" + id, {
+            method: "DELETE",
+            headers: {
+                "Content-Type" : "application/json"
             }
+        })
+        .then((res)=> {
+            if(!res.ok) throw new Error("failed to delete todo")
+                setNewTask(tasks.filter((task)=> task.id !== id))
+        })
+    }
 
-            // Update state locally after successful deletion
-            deletetask()
-        } catch (error) {
-            console.error(error);
-            alert("Error deleting task. Please try again.");
-        }
-    };
+    const deleteUser = async () => {
+        fetch(API_URL + "/users/cintilmon", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => {
+                if (!res.ok) throw new Error("failed to delete this user")
+                createUsers()
+                setNewTask([])
+            })
+            .catch(error => console.log(error))
+    }
 
 
+    // const deleteTasks = (id) => {
+    //     const upDateTask = tasks.filter((task) => task.id !== id);
+    //     setNewTask(upDateTask)
+    // }
 
     return (
         <div className="container">
-
             <div className="form">
                 <p> todos </p>
-
                 <ol>
                     <li className="form-control input-form justify-content-between">
                         <input className=""
                             type="text" placeholder="What needs to be done?"
                             value={imputField}
-                            onChange={imputChange}
+                            onChange={(e) => setImputfield(e.target.value)}
                             onKeyDown={handleTouch}
-
                         />
-                        <button className="add-button" onClick={addTask}>Add</button>
                     </li>
                     {
                         tasks.map((task) =>
@@ -130,12 +121,12 @@ const ToddoList = () => {
                                 <button className="delete-button" onClick={() => deleteTask(task.id)} >X</button>
                             </li>
                         )
+
                     }
                     <li className="form-control length"> {tasks.length} item left</li>
                 </ol>
             </div>
-
-
+            <button onClick={deleteUser}>delete</button>
         </div>
     )
 }
